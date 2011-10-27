4b8b06faafeca95b75278cc12164e73561ff1087
dgaudet86 <dgaudet86@9b412eb8-cd5c-11de-b49f-713b138df6af>
2011-10-27 03:19:32 +0000

    encoding user entered text before using it in urls

--- services.py
+++ services.py
@@ -11,9 +11,11 @@ API_KEY = '9k5g4bqucenr9ztnh9x693cw'
 class YellowpagesBusinessSearchService:
 	def getBusinessesByNameInCity(self, name, city):
 		# http://api.sandbox.yellowapi.com/FindBusiness/?what=scrapbook%20studio&where=saskatoon&fmt=JSON&pgLen=10&apikey=9k5g4bqucenr9ztnh9x693cw&UID=127.0.0.1
-		url = BASE_URL + '/FindBusiness/?what=' + name + '&where=' + city + '&fmt=JSON&pgLen=10&apikey=' + API_KEY + '&UID=127.0.0.1'
-		result = simplejson.load(urllib.urlopen(url))
+		encodedName = urllib.quote(name)
+		encodedCity = urllib.quote(city)
+		url = BASE_URL + '/FindBusiness/?what=' + encodedName + '&where=' + encodedCity + '&fmt=JSON&pgLen=10&apikey=' + API_KEY + '&UID=127.0.0.1'
 		logging.info("called simplejson.load " + url)
+		result = simplejson.load(urllib.urlopen(url))
 		listings = result['listings']	
 		businesses = []
 		for listing in listings:
