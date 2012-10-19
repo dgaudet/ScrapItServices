b842d2b1a6a7413bea9374baeab592ccb3d59c69
dgaudet86 <dgaudet86@9b412eb8-cd5c-11de-b49f-713b138df6af>
2012-10-19 03:24:27 +0000

    - adding ability to search by city with no store name

--- services.py
+++ services.py
@@ -7,6 +7,7 @@ from google.appengine.ext import db
 
 BASE_URL = 'http://api.sandbox.yellowapi.com'
 API_KEY = '9k5g4bqucenr9ztnh9x693cw'
+SEARCH_TERM = 'scrapbook'
 
 class YellowpagesBusinessSearchService:
 	def getBusinessesByNameInCity(self, name, city):
@@ -15,6 +16,35 @@ class YellowpagesBusinessSearchService:
 		encodedCity = urllib.quote(city)
 		businesses = []
 		url = BASE_URL + '/FindBusiness/?what=' + encodedName + '&where=' + encodedCity + '&fmt=JSON&pgLen=10&apikey=' + API_KEY + '&UID=127.0.0.1'
+		logging.info("called simplejson.load " + url)
+		result = simplejson.load(urllib.urlopen(url))
+		if 'listings' in result:
+			listings = result['listings']		
+			for listing in listings:
+				business = Business()
+				business.yellowpages_id = listing['id']
+				business.name = listing['name']
+				if 'address' in listing:
+					if listing['address']:
+						address = listing['address']
+						business.city = address['city']
+						business.province = address['prov']
+						business.country = 'Canada'
+						business.street = address['street']
+				if 'geoCode' in listing:
+					if listing['geoCode']:
+						geolocation = listing['geoCode']
+						lat = geolocation['latitude']
+						lon = geolocation['longitude']
+						business.geolocation = str(lat) + ', ' + str(lon)
+				businesses.append(business)
+		return businesses
+
+	def getBusinessesByCity(self, city):
+		# http://api.sandbox.yellowapi.com/FindBusiness/?what=scrapbook%20studio&where=saskatoon&fmt=JSON&pgLen=10&apikey=9k5g4bqucenr9ztnh9x693cw&UID=127.0.0.1
+		encodedCity = urllib.quote(city)
+		businesses = []
+		url = BASE_URL + '/FindBusiness/?what=' + SEARCH_TERM + '&where=' + encodedCity + '&fmt=JSON&pgLen=10&apikey=' + API_KEY + '&UID=127.0.0.1'
 		logging.info("called simplejson.load " + url)
 		result = simplejson.load(urllib.urlopen(url))
 		if 'listings' in result:
@@ -56,7 +86,10 @@ class BusinessService:
 		return Business_Repository().getBusinessByYellowPagesId(yellowpages_id)
 		
 	def getBusinessesByNameInCity(self, name, city):
-		yellowpages_businesses = YellowpagesBusinessSearchService().getBusinessesByNameInCity(name, city)
+		if not name:
+			yellowpages_businesses = YellowpagesBusinessSearchService().getBusinessesByCity(city)
+		else:
+			yellowpages_businesses = YellowpagesBusinessSearchService().getBusinessesByNameInCity(name, city)
 		combined_businesses = []
 		for business in yellowpages_businesses:
 			repo_business = Business_Repository().getBusinessByYellowPagesId(business.yellowpages_id)
