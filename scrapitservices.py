4038f04d56d432e8728d7c721d01b758afdbdbca
dgaudet86 <dgaudet86@gmail.com>
2013-04-10 01:17:23 +0000

    - refactoring scrapitservices.py to have an update handler

--- scrapitservices.py
+++ scrapitservices.py
@@ -9,55 +9,77 @@ from appsettings import AppSettingsService
 from services import YellowPages_BusinessService
 
 class YellowpagesBusinessSearch(webapp2.RequestHandler):
-    def get(self):
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-        
-        template_values = {
-            'user': users.get_current_user(),
-            'url_linktext': url_linktext,
-        }
+	def get(self):
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
 
-        path = os.path.join(os.path.dirname(__file__), 'yellowpagesbusinessess.html')
-        self.response.out.write(template.render(path, template_values))
+		template_values = {
+			'user': users.get_current_user(),
+			'url_linktext': url_linktext,
+		}
 
-    
-    def post(self):
-      logging.info('***YellowpagesBusinessSearchHandler post')
-      form_type = cgi.escape(self.request.get('post_type'))
-      if form_type == 'add':
-          yellowpages_id = cgi.escape(self.request.get('yellowpages_id'))
-          url = cgi.escape(self.request.get('url'))
-          YellowPages_BusinessService().updateBusinessUrl(yellowpages_id, url)
-      name = cgi.escape(self.request.get('name'))
-      city = cgi.escape(self.request.get('city'))
-      businesses = YellowPages_BusinessService().getBusinessesByNameInCity(name, city)
-      
-      if users.get_current_user():
-          url = users.create_logout_url(self.request.uri)
-          url_linktext = 'Logout'
-      else:
-          url = users.create_login_url(self.request.uri)
-          url_linktext = 'Login'
-      
-      template_values = {
-          'user': users.get_current_user(),
-          'search_perfomred': True,
-          'businesses': businesses,
-          'url': url,
-          'url_linktext': url_linktext,
-          'search_name': name,
-          'search_city': city
-      }
-      path = os.path.join(os.path.dirname(__file__), 'yellowpagesbusinessess.html')
-      self.response.out.write(template.render(path, template_values))
+		path = os.path.join(os.path.dirname(__file__), 'yellowpagesbusinessess.html')
+		self.response.out.write(template.render(path, template_values))
+
+	def post(self):
+		name = cgi.escape(self.request.get('name'))
+		city = cgi.escape(self.request.get('city'))
+		businesses = YellowPages_BusinessService().getBusinessesByNameInCity(name, city)
+
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+
+		template_values = {
+			'user': users.get_current_user(),
+			'search_performed': True,
+			'businesses': businesses,
+			'search_name': name,
+			'search_city': city
+		}
+		path = os.path.join(os.path.dirname(__file__), 'yellowpagesbusinessess.html')
+		self.response.out.write(template.render(path, template_values))
+
+class UpdateYellowPagesBusiness(webapp2.RequestHandler):
+	def post(self):
+		form_type = cgi.escape(self.request.get('post_type'))
+		if form_type == 'add':
+			yellowpages_id = cgi.escape(self.request.get('yellowpages_id'))
+			url = cgi.escape(self.request.get('url'))
+			YellowPages_BusinessService().updateBusinessUrl(yellowpages_id, url)
+
+		name = cgi.escape(self.request.get('name'))
+		city = cgi.escape(self.request.get('city'))
+		businesses = YellowPages_BusinessService().getBusinessesByNameInCity(name, city)
+
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+
+		template_values = {
+			'user': users.get_current_user(),
+			'search_performed': True,
+			'businesses': businesses,
+			'url': url,
+			'url_linktext': url_linktext,
+			'search_name': name,
+			'search_city': city
+		}
+		path = os.path.join(os.path.dirname(__file__), 'yellowpagesbusinessess.html')
+		self.response.out.write(template.render(path, template_values))
 
 app = webapp2.WSGIApplication([
   ('/scrapitservices/', YellowpagesBusinessSearch),
-  ('/scrapitservices/update', YellowpagesBusinessSearch)
+  ('/scrapitservices/update', UpdateYellowPagesBusiness)
   
 ], debug=AppSettingsService().appInDebugMode())
